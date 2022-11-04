99bfb4228df2a1e4213ed8b8c6e267f5aa7555e2
JaredJustJared <116468705+JaredJustJared@users.noreply.github.com>
2022-11-04 16:29:35 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,46 @@
+console.log("Hello World");
+const adminAge = 34; // number
+let adminGreeting = 'Hello'; //string
+let adminName; // indefined
+let adminSecondName = null; // null
+let isAdmin = true;
+let isNotAdmin = false; // boolean
+let admin = {
+    surname: "Shashkin",
+    age: 35
+}; // object
+const bigint = 24574857348634063485037475930n; // bigint
+let id = Symbol("id"); // symbol
+
+const newAdmin = {
+    surname: "Novoselova",
+    age: 24
+}
+
+var newAnotherAdmin = {
+    surname: "Ivanov",
+    age: 49
+}
+
+console.log(adminAge);
+console.log(adminGreeting);
+console.log(adminName);
+console.log(adminSecondName);
+console.log(isAdmin);
+console.log(isNotAdmin);
+console.log(admin);
+console.log(id);
+console.log(newAdmin);
+console.log(newAnotherAdmin);
+
+console.log(typeof 31)
+console.log(typeof 'Hello')
+console.log(typeof adminName)
+console.log(typeof null)
+console.log(typeof true)
+console.log(typeof {surname: "Shashkin",
+age: 31})
+console.log(typeof 24574857348634063485037475930n)
+console.log(typeof id)
+
+
